04d1688d24b30fc10180da8c70953f964f96e267
UyTran <vanuy113@gmail.com>
2017-08-01 11:36:12 +0700

    nothing

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {FormComponent } from './form.component';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild(FormComponent ) form: FormComponent;
 trainers: any[] = [
     { name: 'Kien', birthday: '1992', team: 'Ruby', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8ARvRFLoWGkEmneyiZ6Yj-XDtwRG4ko2wxV5bzaaM1krpT40fng' },
     { name: 'Vi', birthday: '1989', team: 'FE', avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA95wKwihQ4G1QxiFWWnvKSp5__Yr6a1ZjLxk-9kIiKzq6df_h' },
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TrainersComponent } from './trainers.component';
 import { EmptyDirective } from './empty.directive';
+import { FormComponent } from './form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TrainersComponent,
-    EmptyDirective
+    EmptyDirective,
+    FormComponent
   ],
   imports: [
     BrowserModule
