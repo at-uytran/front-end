9fa2562fd270658abb241549eb5525a36459f182
UyTran <vanuy113@gmail.com>
2017-08-07 14:58:07 +0700

    angular form

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
+import { MemberFormComponent } from './member_form.component';
+import { NgForm, FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name: string = '';
+  teams: any[];
+  birthday: string = '';
+  constructor() {
+    this.teams = ['Ruby', 'FE', 'PHP'];
+  }
+onChange(){
+
+}
+
+  addMember(form: NgForm) {
+  var a = document.getElementById("list");
+  var b = document.createElement('li');
+  var c = document.createElement('span');
+  c.innerHTML = "name: "+ form.value.name + ", birthday:" +form.value.birthday;
+  b.appendChild(c);
+  a.appendChild(b);
+  }
 }
